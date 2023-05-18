bc5403c5126b6d51368435d65678e9b3cb4b6523
Tushar Hatwar <60131764+tushar-hatwar@users.noreply.github.com>
2023-05-18 09:25:22 +0530

    Update mount_adls_storage.py

--- adhoc/mount_adls_storage.py
+++ adhoc/mount_adls_storage.py
@@ -1,9 +1,9 @@
 # Databricks notebook source
 # Modified Client ID
 storage_account_name = "financestoragebig2023"
-client_id            = "369c15e7-74ea-447d-8348-b088453c3ee5"
-tenant_id            = "a9a1ad60-0851-40df-8e61-d9dd36cf0e8b"
-client_secret        = "OI78Q~lRMsO-7ICQwe5sKY3CZka3YaXVsBkSWbMl"
+client_id            = ""
+tenant_id            = ""
+client_secret        = ""
 
 # COMMAND ----------
 
